ec3373b3bc88e030a7b71687a0a92ceb595b4a83
Jungieve <zihan.cong@gmail.com>
2016-07-02 22:14:13 +0800

    链表回文stack解法

--- src/List/Palindrome.java
+++ src/List/Palindrome.java
@@ -1,5 +1,7 @@
 package List;
 
+import java.util.Stack;
+
 /**
  * Created by root on 2016/6/30.
  */
@@ -7,31 +9,21 @@ public class Palindrome {
     public boolean isPalindrome(ListNode pHead) {
         // write code here
         if(pHead == null)	return false;
-        //获取链表长度
-        ListNode pLength = pHead;
-        //记录链表长度
-        int length = 0;
-        while(pLength!=null){
-            pLength = pLength.next;
-            length++;
+        ListNode fast = pHead;
+        ListNode slow = pHead;
+        while(fast != null && fast.next !=null){
+            fast = fast.next.next;
+            slow = slow.next;
         }
-        //额外链表
-        ListNode extra = new ListNode(0);
-        ListNode dummy = extra;
-
-        //移动到extra
-        for(int i = 0;i<length/2;i++){
-            ListNode temp = new ListNode(pHead.val);
-            temp.next = dummy;
-            dummy = temp;
+        Stack<Integer> stack = new Stack();
+        while(slow!=null){
+            stack.push(slow.val);
+            slow = slow.next;
+        }
+        while(stack.size()!=0 && stack.peek() == pHead.val ){
+            stack.pop();
             pHead = pHead.next;
         }
-        pHead = (length%2 == 0)?pHead:(pHead.next);
-        while(dummy!=null && pHead!= null && dummy.val == pHead.val){
-            dummy = dummy.next;
-            pHead = pHead.next;
-        }
-        return (pHead == null);
-
+        return (stack.size() <= 1);
     }
 }
