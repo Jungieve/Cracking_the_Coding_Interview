3ec25154f42afef0be48859bbba913dac17dd0f6
Jungieve <zihan.cong@gmail.com>
2016-07-03 14:06:59 +0800

    将listnode变量修改为public

--- src/List/ListNode.java
+++ src/List/ListNode.java
@@ -4,9 +4,8 @@ package List;
  * Created by ZihanCong on 16/6/14.
  */
 public class ListNode<T> {
-    T val;
-    ListNode next = null;
-
+    public T val;
+    public ListNode next = null;
     public ListNode(T val) {
         this.val = val;
     }
