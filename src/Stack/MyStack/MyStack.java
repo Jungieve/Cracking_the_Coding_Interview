5a51ec4389e523e6f24c29c6a91bb1e724c15706
Jungieve <zihan.cong@gmail.com>
2016-07-03 13:31:52 +0800

    增加Mystack接口以及数组,链表的实现结构

---
+++ src/Stack/MyStack/MyStack.java
@@ -0,0 +1,15 @@
+package Stack.MyStack;
+
+
+/**
+ * Created by ZihanCong on 16/7/3.
+ * 链表实现一个栈
+ */
+public interface MyStack<T> {
+    boolean isEmpty();
+    void clear();
+    int length();
+    T push(T item);
+    T pop(T item);
+    T peek();
+}
---
+++ src/Stack/MyStack/MyStackByListImpl.java
@@ -0,0 +1,36 @@
+package Stack.MyStack;
+
+/**
+ * Created by ZihanCong on 16/7/3.
+ */
+public class MyStackByListImpl<T> implements MyStack<T> {
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public int length() {
+        return 0;
+    }
+
+    @Override
+    public T push(T item) {
+        return null;
+    }
+
+    @Override
+    public T pop(T item) {
+        return null;
+    }
+
+    @Override
+    public T peek() {
+        return null;
+    }
+}
