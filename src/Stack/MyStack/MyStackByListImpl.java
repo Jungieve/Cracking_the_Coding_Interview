60f50e355b8ad4d09cc166968fe188eb4bcba07a
Jungieve <zihan.cong@gmail.com>
2016-07-03 14:20:30 +0800

    实现利用链表完成的一个简单的栈

--- src/Stack/MyStack/MyStackByListImpl.java
+++ src/Stack/MyStack/MyStackByListImpl.java
@@ -1,36 +1,59 @@
 package Stack.MyStack;
 
+import List.ListNode;
+
+import java.util.EmptyStackException;
+import java.util.LinkedList;
+
 /**
  * Created by ZihanCong on 16/7/3.
  */
 public class MyStackByListImpl<T> implements MyStack<T> {
+    private int size;
+    private ListNode top;
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
-
     @Override
     public void clear() {
-
+        top = null;
+        size = 0;
     }
 
     @Override
     public int length() {
-        return 0;
+        return size;
     }
 
     @Override
     public T push(T item) {
-        return null;
+        //push时候,每次把新的结点增加到链表前面
+        ListNode listNode = new ListNode(item);
+        //如果没有元素
+        if(top == null)
+            top = listNode;
+        else{
+            listNode.next = top;
+            top = listNode;
+        }
+        size++;
+        return item;
     }
 
     @Override
     public T pop(T item) {
-        return null;
+        //通过调用peek函数预先判断stack是否为空
+        T obj = peek();
+        top = top.next;
+        size--;
+        return obj;
     }
 
     @Override
     public T peek() {
-        return null;
+        if(size == 0)
+            throw new EmptyStackException();
+        return (T)top.val;
     }
 }
