9b849e2b17630e3937fef034997080903bf20b2c
Jungieve <zihan.cong@gmail.com>
2016-07-13 16:58:29 +0800

    修正最近公共祖先（二叉搜索树）暴力解法版 当u,v在一颗树上，比如u是v的父节点，则LCA还是u

--- src/Tree_Graph/LCA_BST_rough.java
+++ src/Tree_Graph/LCA_BST_rough.java
@@ -14,21 +14,14 @@ public class LCA_BST_rough {
         }
         int left = u.val;
         int right = v.val;
-        TreeNode parent = null;
         while(true){
             //如果parent小于u,v，往parent的右子树查找
             if(root.val < left){
-                parent = root;
                 root = root.right;
             }
             //如果parent大于u,v，往parent的左子树查找
-            else if(root.val < left){
-                parent = root;
+            else if(root.val > right){
                 root = root.left;
-            }
-            //如果v是u的祖先，返回u的父节点
-            else if(root.val == left || root.val == right){
-                return parent.val;
             }
             //当 left<root.val<right 说明u和v分居在t的两侧，则当前结点root为最近公共节点
             else return root.val;
