32e3030296527816e8eaf2283a273025f6686e36
Jungieve <zihan.cong@gmail.com>
2016-07-16 20:48:21 +0800

    二进制插入

---
+++ src/Bit/BinInsert.java
@@ -0,0 +1,11 @@
+package Bit;
+
+/**
+ * Created by root on 2016/7/16.
+ */
+public class BinInsert {
+    public int binInsert(int n, int m, int j, int i) {
+        // write code herem
+        return n + (m<<j);
+    }
+}
