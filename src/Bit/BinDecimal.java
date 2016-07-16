19572e0f83827dc11adeb3e04c597161c796a6fe
Jungieve <zihan.cong@gmail.com>
2016-07-16 21:01:47 +0800

    二进制小数

---
+++ src/Bit/BinDecimal.java
@@ -0,0 +1,26 @@
+package Bit;
+
+/**
+ * Created by root on 2016/7/16.
+ */
+public class BinDecimal {
+    public String printBin(double num) {
+        // write code here
+        StringBuffer result = new StringBuffer();
+        result.append('0');
+        result.append('.');
+        for(int i = 0;i<8;i++){
+            num *= 2;
+            if(num >= 1){
+                num -= 1;
+                result.append('1');
+            }
+            else if(num == 0)
+                break;
+            else
+                result.append('0');
+
+        }
+        return (num == 0)?result.toString():"Error";
+    }
+}
