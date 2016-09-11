c67c1cc5894dca115a32522ce5bafe04a3210662
Jungieve <zihan.cong@gmail.com>
2016-09-11 13:59:14 +0800

    实现迭代器

--- src/Techs/Java/CircularArray.java
+++ src/Techs/Java/CircularArray.java
@@ -1,9 +1,11 @@
 package Techs.Java;
 
+import java.util.Iterator;
+
 /**
  * Created by root on 2016/9/11.
  */
-public class CircularArray<T> {
+public class CircularArray<T> implements Iterable<T>{
     private T[] items;
     private int head = 0;
     //为了无法解决泛型数组，必须将一个Object[]强制转型为T[]
@@ -28,4 +30,31 @@ public class CircularArray<T> {
     public void set(int i,T item) {
         items[convert(i)] = item;
     }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new CircularArrayIterator<T>(this);
+    }
+    private class CircularArrayIterator<T1> implements Iterator{
+        //_current反应从旋转后的开头算起的偏移量，而不是从原始数组的开头开始算
+        private int _current = -1;
+        private T1[] _items;
+        public CircularArrayIterator(CircularArray<T1> ts) {
+            _items = ts.items;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return _current < items.length-1;
+        }
+
+        @Override
+        public Object next() {
+           _current++;
+            T1 item = (T1) _items[convert(_current)];
+            return item;
+        }
+
+    }
 }
+
